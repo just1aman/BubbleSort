74094c4ccb0722d824f5351b75790db36341d167
just1aman <just1aman@gmail.com>
2021-10-27 02:02:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function bubblesort(array)
+{
+   for(j=1;j<array.length;j++)
+  {
+    for(i=0;i<array.length-j;i++)
+  {
+    
+      if(array[i]>array[i+1])
+      {
+        let greaterValue=array[i]
+        array[i]= array[i+1]
+        array[i+1]=greaterValue
+      }
+      
+   // }
+  }
+}
+  return array
+}
+
+let randomArray=[11,4,12,7,5,9,0]
+bubblesort(randomArray)
